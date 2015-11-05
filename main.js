2ed679f8a055786dc28fcd7025048a157aca8e87
Mary Ellen Klein <maryellenklein@gmail.com>
2015-11-05 16:35:47 -0600

    Twitter Project - Commit 3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var Router = Backbone.Router.extend({
+//left side: displayed in brower. right side: fucnction to call.
+var AppRouter = Backbone.Router.extend({
   routes: {
     ''          :'home',
     'login'     :'login',
@@ -7,8 +8,43 @@ var Router = Backbone.Router.extend({
     'users'     :'users', //need to make function called users
     'user/:id'  :'userId'  //need to make function called userId
   },
-})
 
+  home: function(){
+    tweetsCollection.fetch({
+      success: function(){
+        $('main').html(feedView.el);
+      }
+    });
+  },
+
+  dashboard: function(){
+    tweetsCollection.fetch({
+      success: function(){
+        $('main').html(feedView.el);
+      }
+    });
+  },
+
+  userId: function(){
+    tweetsCollection.fetch({
+      success: function(){
+        $('main').html(feedView.el);
+      }
+    });
+  },
+
+  login: function(){
+    var loginForm = new FormView();
+    loginForm.render()
+    $('main').html(loginForm.el);
+
+  }
+
+
+})
+// $('nav a').click(function(){
+//
+// });
 
 // $.ajaxSetup({
 //  headers: {
@@ -20,7 +56,7 @@ var Router = Backbone.Router.extend({
 // Make a new model.
 var Tweet = Backbone.Model.extend({
   // When we make a new Tweet,
-  // this will use the POST Method to create a new tweet on this URL.
+  // this will use the GET Method to print a new tweet from this API.
   url: 'http://tiny-starburst.herokuapp.com/collections/chat'
 });
 
@@ -28,7 +64,14 @@ var Tweet = Backbone.Model.extend({
 var Tweets = Backbone.Collection.extend({
   model: Tweet,
   url: 'http://tiny-starburst.herokuapp.com/collections/chat'
-})
+});
+
+// Make a new Model for the login and register pages:
+var Form1 = Backbone.Model.extend({
+  //When we click the login button,
+  // this will use the POST method to send the form info to the server.
+  url: 'http://tiny-starburst.herokuapp.com/collections/chat'
+});
 
 
 
@@ -37,7 +80,6 @@ var TweetView = Backbone.View.extend({
   tagName: 'article',
   className: 'tweet',
   template: _.template($('#tweetViewTemplate').html()),
-
 
   render: function(){
     var data = this.model.toJSON();
@@ -58,6 +100,7 @@ var FeedView = Backbone.View.extend({
   render: function(){
     var view = this;
 
+    this.$el.html('');
     this.collection.each(function(model){
       var tweet = new TweetView({
         model: model
@@ -68,6 +111,19 @@ var FeedView = Backbone.View.extend({
   }
 });
 
+//Make a new view for the form.
+var FormView = Backbone.View.extend({
+  tagName: 'section',
+  className: 'form',
+  template: _.template($('#inputBoxTemplate').html()),
+
+  render: function(){
+    this.$el.html(this.template());
+    return this;
+  }
+});
+
+
 // creates new instance of collected tweets
 var tweetsCollection = new Tweets();
 
@@ -76,11 +132,7 @@ var feedView = new FeedView({
   collection: tweetsCollection
 });
 
-tweetsCollection.fetch({
-  success: function(){
-    $('main').append(feedView.el);
-  }
-});
-//
-// var router = new Router();   //tells Backbone to listen for changes
-// Backbone.history.start();
+
+
+var router = new AppRouter();   //tells Backbone to listen for changes
+Backbone.history.start();
