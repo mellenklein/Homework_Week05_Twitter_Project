af53f21e73d90945e781be43268571a7e0c8fd7d
Freddy Roberts <FreddyRoberts9@gmail.com>
2015-11-07 16:25:28 -0600

    updated

--- main.js
+++ main.js
@@ -67,13 +67,13 @@ var AppRouter = Backbone.Router.extend({
 var Tweet = Backbone.Model.extend({
   // When we make a new Tweet,
   // this will use the GET Method to print a new tweet from this API.
-  url: 'https://twitter-pi.herokuapp.com/users/?include=tweets'
+  url: 'https://twittertiy.herokuapp.com/users'
 });
 
 // Make a new collection.
 var Tweets = Backbone.Collection.extend({
   model: Tweet,
-  url: 'https://twitter-pi.herokuapp.com/users/?include=tweets'
+  url: 'https://twittertiy.herokuapp.com/users'
 });
 
 var Dashboard = Backbone.Collection.extend({
